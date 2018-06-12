b3d329d8fe0614712d73aedd7c0b71f546527949
mp5217 <michal.pandy17@imperial.ac.uk>
2018-06-12 15:24:09 +0100

    events structs

--- examples/dummy_game/example_game.c
+++ examples/dummy_game/example_game.c
@@ -28,21 +28,45 @@ typedef struct game {
   game_sprite_t game_sprite;
 } game_t;
 
-typedef struct events {
+typedef struct shoot_event {
+  //participants
+  player_t *player;
+  shot_t *shot;
 
-} events_t;
+  bool (*conditions[])(game_t, player_t, shot_t);
+  void (*actions[])(game_t, player_t, shot_t);
+} shoot_event_t;
 
-/*Actions and their corresponding conditions*/
+typedef struct shot_event {
+  //participants
+  player_t *player;
+  shot_t *shot;
 
-void shoot_action(player_t *player, game_t *game) {
-  //new_object("shot", player, game);
+  bool (*conditions[])(game_t, player_t, shot_t);
+  void (*actions[])(game_t, player_t, shot_t);
+} shot_event_t;
+
+typedef struct game_slowdown_event {
+  //participants
+  game_t *game;
+
+  bool (*conditions[])(game_t);
+  void (*actions[])(game_t);
+} game_event_t;
+
+
+
+/*Event creating actions and their corresponding conditions*/
+
+void *shoot_action(player_t *player, game_t *game) {
+  new_object("shot", player, game);
 }
 
 void decrease_health(player_t *player, int val) {
   player->health -= val;
 }
 
-void shot_action(player_t *player, shot_t *shot, game_t game) {
+event_t shot_action(player_t *player, shot_t *shot, game_t game) {
   decrease_health(player, 5);
   if(player->health <= 0) {
     //destroy_object(player);
@@ -57,11 +81,11 @@ int tree_collision_cond2(player_t *p, tree_t *tree, game_t *game) {
   return tree->height > 100;
 }
 
-void tree_collided_action(player_t *p, tree_t *tree, game_t *game) {
+event_t tree_collided_action(player_t *p, tree_t *tree, game_t *game) {
   decrease_health(p, tree->height/3);
 }
 
-void game_slowdown_action(player_t *p, tree_t *tree, game_t *game) {
+event_t game_slowdown_action(player_t *p, tree_t *tree, game_t *game) {
   //game->speed -= 5;
 }
 
