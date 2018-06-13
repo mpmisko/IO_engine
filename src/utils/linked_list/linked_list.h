96423a7e7e262d235bc1d5b55f757cc4fe2ef8ef
mp5217 <michal.pandy17@imperial.ac.uk>
2018-06-14 00:28:51 +0100

    create functions and listeners

---
+++ src/utils/linked_list/linked_list.h
@@ -0,0 +1,37 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stddef.h>
+
+typedef struct linked_list_node {
+  void *data;
+  struct linked_list_node *next;
+} List_Node;
+
+/* Returns empty head node that can be used as list */
+List_Node *get_new_list();
+
+/*
+ * Arguments:
+ * head = head of the list we want to insert to
+ * data = data we want to insert to new node
+ * size = size of the data we are inserting
+ * Append new node to the list given by head.
+ * Will reach the end if given list, create new node there and insert data into
+ * that node. return new tail of the list
+ */
+List_Node *append(List_Node *head, void *data, size_t size);
+
+/*
+ * safely deletes whole list starting from head
+ * WARNING: should be only used on head obtained by get_new_list()
+ */
+void delete_list(List_Node *head);
+
+/*
+ * Iterates until the end of the list and returns pointer to the last node of
+ * the list
+ */
+List_Node *get_tail(List_Node *head);
+
+#endif
